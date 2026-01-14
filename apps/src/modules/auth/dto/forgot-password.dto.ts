0c9d42eeccd1454cfa131e1547b9d6c43c6d948c
ikokywu <rikiridwan949@gmail.com>
2026-01-14 21:07:17 +0700

    refactor: adjust dto

--- apps/src/modules/auth/dto/forgot-password.dto.ts
+++ apps/src/modules/auth/dto/forgot-password.dto.ts
@@ -1,7 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional } from 'class-validator';
+import { IdentifierType } from './login.dto';
 
 export class ForgotPasswordDto {
+  @ApiProperty({ example: 'Email', enum: IdentifierType })
+  @IsString()
+  identifierType: IdentifierType;
+
   @ApiProperty({ description: 'email or phone number to look up user', example: 'user@example.com' })
   @IsString()
   identifier: string;

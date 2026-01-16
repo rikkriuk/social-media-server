425812611f84c64675f0cc5ee3ebdf83114bca89
ikokywu <rikiridwan949@gmail.com>
2026-01-16 21:41:17 +0700

    feat: add comment model

---
+++ apps/src/modules/comment/comment.entity.ts
@@ -0,0 +1,72 @@
+import {
+   Table,
+   Column,
+   Model,
+   DataType,
+   ForeignKey,
+   BelongsTo,
+   CreatedAt,
+   UpdatedAt,
+} from 'sequelize-typescript';
+import { Post } from '../post/post.entity';
+import { Profile } from '../profile/profile.model';
+
+@Table({
+   tableName: 'comments',
+   timestamps: true,
+   underscored: true,
+})
+export class Comment extends Model {
+   @Column({
+      type: DataType.UUID,
+      defaultValue: DataType.UUIDV4,
+      primaryKey: true,
+   })
+   id: string;
+
+   @ForeignKey(() => Post)
+   @Column({
+      type: DataType.UUID,
+      allowNull: false,
+   })
+   postId: string;
+
+   @BelongsTo(() => Post)
+   post: Post;
+
+   @ForeignKey(() => Profile)
+   @Column({
+      type: DataType.UUID,
+      allowNull: false,
+   })
+   profileId: string;
+
+   @BelongsTo(() => Profile)
+   profile: Profile;
+
+   @Column({
+      type: DataType.TEXT,
+      allowNull: false,
+   })
+   content: string;
+
+   @Column({
+      type: DataType.INTEGER,
+      defaultValue: 0,
+   })
+   likesCount: number;
+
+   @CreatedAt
+   @Column({
+      type: DataType.DATE,
+      defaultValue: DataType.NOW,
+   })
+   createdAt: Date;
+
+   @UpdatedAt
+   @Column({
+      type: DataType.DATE,
+      defaultValue: DataType.NOW,
+   })
+   updatedAt: Date;
+}

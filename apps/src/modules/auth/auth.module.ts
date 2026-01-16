a28b4184a51f67822056860aacc01a7d878e79ee
ikokywu <rikiridwan949@gmail.com>
2026-01-16 21:39:14 +0700

    refactor: adjust jwt stregy and passport module

--- apps/src/modules/auth/auth.module.ts
+++ apps/src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { User } from '../users/user.model';
 import { OtpCode } from '../otp/otp.model';
 import { Profile } from '../profile/profile.model';
@@ -10,12 +12,13 @@ import { Profile } from '../profile/profile.model';
 @Module({
   imports: [
     SequelizeModule.forFeature([User, OtpCode, Profile]),
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'change_this_secret',
       signOptions: { expiresIn: '7d' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })

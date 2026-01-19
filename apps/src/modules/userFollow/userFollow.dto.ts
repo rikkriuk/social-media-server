4fe160b1f2608ab008207c939274a4587fd59483
ikokywu <rikiridwan949@gmail.com>
2026-01-19 21:13:05 +0700

    feat: add suggestion, search and user follow dto

--- apps/src/modules/userFollow/userFollow.dto.ts
+++ apps/src/modules/userFollow/userFollow.dto.ts
@@ -1,5 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsUUID } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString, IsUUID } from 'class-validator';
 import { PaginationDto } from '../../common/pagination.dto';
 
 export class CreateFollowDto {
@@ -16,11 +16,38 @@ export class CreateUnfollowDto extends CreateFollowDto {}
 
 export class GetFollowersDto extends PaginationDto {
   @ApiProperty({ description: 'ID of the user whose followers are to be retrieved (UUID)' })
-  @IsUUID()
+  @IsString()
   userId: string;
 }
 
 export class GetFollowingDto extends GetFollowersDto {}
+
+export class GetSuggestionsDto extends PaginationDto {
+  @ApiProperty({ description: 'ID of the current user (UUID)' })
+  @IsString()
+  userId: string;
+}
+
+export class SearchUsersDto extends PaginationDto {
+  @ApiProperty({ description: 'Search query for username or name' })
+  @IsString()
+  search: string;
+
+  @ApiPropertyOptional({ description: 'ID of the current user to exclude from results (UUID)' })
+  @IsOptional()
+  @IsString()
+  currentUserId?: string;
+}
+
+export class CheckFollowDto {
+  @ApiProperty({ description: 'ID of the follower (UUID)' })
+  @IsString()
+  followerId: string;
+
+  @ApiProperty({ description: 'ID of the user being followed (UUID)' })
+  @IsString()
+  followingId: string;
+}
 
 export enum FollowType {
   FOLLOWERS = 'followers',

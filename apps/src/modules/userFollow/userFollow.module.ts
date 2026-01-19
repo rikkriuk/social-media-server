b3b544c8c4c59cff59e8c707b4372aad28ec00b5
ikokywu <rikiridwan949@gmail.com>
2026-01-19 21:11:32 +0700

    feat: add user model

--- apps/src/modules/userFollow/userFollow.module.ts
+++ apps/src/modules/userFollow/userFollow.module.ts
@@ -3,10 +3,12 @@ import { UserFollowController } from "./userFollow.controller";
 import { UserFollowService } from "./userFollow.service";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UserFollow } from "./userFollow.entity";
+import { User } from "../users/user.model";
 
 @Module({
    imports: [SequelizeModule.forFeature([
       UserFollow,
+      User,
    ])],
    controllers: [UserFollowController],
    providers: [UserFollowService],

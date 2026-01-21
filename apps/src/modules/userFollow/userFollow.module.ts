6d65485a1bbf8802e62cc9dea29343ae2706fbe1
ikokywu <rikiridwan949@gmail.com>
2026-01-21 16:02:58 +0700

    refactor: feature

--- apps/src/modules/userFollow/userFollow.module.ts
+++ apps/src/modules/userFollow/userFollow.module.ts
@@ -4,12 +4,18 @@ import { UserFollowService } from "./userFollow.service";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { UserFollow } from "./userFollow.entity";
 import { User } from "../users/user.model";
+import { Profile } from "../profile/profile.model";
+import { NotificationModule } from "../notification/notification.module";
 
 @Module({
-   imports: [SequelizeModule.forFeature([
-      UserFollow,
-      User,
-   ])],
+   imports: [
+      SequelizeModule.forFeature([
+         UserFollow,
+         User,
+         Profile,
+      ]),
+      NotificationModule,
+   ],
    controllers: [UserFollowController],
    providers: [UserFollowService],
    exports: [],

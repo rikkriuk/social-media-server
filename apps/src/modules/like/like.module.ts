fbc62478137cca914da7533214a596e0ced7c291
ikokywu <rikiridwan949@gmail.com>
2026-01-21 16:03:24 +0700

    refactor: feature

--- apps/src/modules/like/like.module.ts
+++ apps/src/modules/like/like.module.ts
@@ -4,9 +4,14 @@ import { LikeController } from './like.controller';
 import { LikeService } from './like.service';
 import { Like } from './like.entity';
 import { Post } from '../post/post.entity';
+import { NotificationModule } from '../notification/notification.module';
+import { Profile } from '../profile/profile.model';
 
 @Module({
-   imports: [SequelizeModule.forFeature([Like, Post])],
+   imports: [
+      SequelizeModule.forFeature([Like, Post, Profile]),
+      NotificationModule,
+   ],
    controllers: [LikeController],
    providers: [LikeService],
    exports: [LikeService],
